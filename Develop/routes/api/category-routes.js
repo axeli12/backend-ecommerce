c1942289afe1761a446ee1b225eb5d22d23d9c1a
Axel Irias <axelirias@Axels-MacBook-Air.local>
2023-03-30 18:10:12 -0600

    added routes to specified methods, that they are doing

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -4,24 +4,48 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
+  Category.findAll({
+    include: [Product],
+  })
+  .then((categories) => res.json(categories))
+  .catch((err) => res.status(500).json(err));
   // find all categories
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
+  Category.findOne({
+    where: { id:req.params.id },
+    include: [Product]
+  })
+  .then((categories) => res.json(categories))
+  .catch((err) => res.status(500).json(err));
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
+  Category.create(req.body)
+  .then((categories) => res.json(categories))
+  .catch((err) => res.status(500).json(err));
   // create a new category
 });
 
 router.put('/:id', (req, res) => {
+  Category.update(req.body, {
+    where: { id:req.params.id }
+  })
+  .then((categories) => res.json(categories))
+  .catch((err) => res.status(500).json(err));
   // update a category by its `id` value
 });
 
 router.delete('/:id', (req, res) => {
+  Category.delete({
+    where: { id:req.params.id }
+  })
+  .then((categories) => res.json(categories))
+  .catch((err) => res.status(500).json(err));
   // delete a category by its `id` value
 });
 
